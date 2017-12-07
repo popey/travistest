124a7c0dd6d6ec1bccbfdfcf0369c8c276e6b853
Alan Pope <alan@popey.com>
2017-12-07 13:30:57 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pulsemixer',
+      version='1.3.0',
+      description='pulsemixer - cli and curses mixer for pulseaudio',
+      url='https://github.com/GeorgeFilipkin/pulsemixer',
+      author='George Filipkin',
+      author_email='botebotebot@gmail.com',
+      license='MIT',
+      scripts=['pulsemixer'],
+      )
